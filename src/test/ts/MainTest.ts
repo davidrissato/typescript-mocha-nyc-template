67c73bd5874e7a62985520174f36e53044941d81
David Rissato Cruz <davidrc@gmail.com>
2017-10-05 13:46:27 -0700

    New version using typemoq

--- src/test/ts/MainTest.ts
+++ src/test/ts/MainTest.ts
@@ -1,36 +1,29 @@
-"use strict";
-
-import "mocha";
-
 // testing framework imports
-import * as sinon from "sinon";
+import "mocha";
+import * as TypeMoq from "typemoq";
 import {expect} from "chai";
-import * as chai from "chai";
-import * as chaiAsPromised from "chai-as-promised";
-chai.use(chaiAsPromised);
 
 // classes under testing
 import Main from '../../main/ts/Main';
-
-
-// sinon sandbox
-let __sandbox;
-
-beforeEach(function () {
-    __sandbox = sinon.sandbox.create();
-});
-
-afterEach(function () {
-    __sandbox.restore();
-});
-
+import MyInterface from '../../main/ts/interfaces/MyInterface';
 
 
 describe("Testing Main", function () {
 
     it("should work", async function () {
-       await expect(Main.run([])).to.eventually.deep.equal({exitCode: 0, results: "OK", error: false});
+        const results = await Main.run([]);
+        await expect(results).to.deep.equal({exitCode: 0, results: "OK", error: false});
     });
 
+    it("should mock", async function () {
+        const mock: TypeMoq.IMock<MyInterface> = TypeMoq.Mock.ofType<MyInterface>();
+        let It = TypeMoq.It;
+        mock.setup(x => x.test(It.isAny())).returns(()=>"ABC");
+        const obj = mock.object;
+
+        const result = obj.test("abc");
+
+        expect(result).equal("ABC");
+    });
 
 });
